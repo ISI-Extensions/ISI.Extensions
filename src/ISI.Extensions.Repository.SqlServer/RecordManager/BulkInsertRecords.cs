66385a785d6fa766de5f67037f93b6e6e376fa42
Ron Muth <ron.muth@isi-net.com>
2021-03-21 01:58:31 -0400

    WIP

--- src/ISI.Extensions.Repository.SqlServer/RecordManager/BulkInsertRecords.cs
+++ src/ISI.Extensions.Repository.SqlServer/RecordManager/BulkInsertRecords.cs
@@ -12,7 +12,7 @@ Redistribution and use in source and binary forms, with or without modification,
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 #endregion
- 
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,12 +26,12 @@ namespace ISI.Extensions.Repository.SqlServer
 {
 	public abstract partial class RecordManager<TRecord>
 	{
-		public virtual void BulkInsertRecords(IEnumerable<TRecord> records, bool keepIdentities = false, int bulkCopyTimeoutInSeconds = 3600, int batchSize = 1000)
+		public virtual void BulkInsertRecords(IEnumerable<TRecord> records, bool keepIdentities = false, int bulkCopyTimeoutInSeconds = 3600, int batchSize = 1000, Action<string> batchLogger = null)
 		{
 			BulkInsertRecords(records, keepIdentities, false, bulkCopyTimeoutInSeconds, batchSize);
 		}
 
-		protected void BulkInsertRecords(IEnumerable<TRecord> records, bool keepIdentities, bool hasArchiveTable, int bulkCopyTimeoutInSeconds = 3600, int batchSize = 1000)
+		protected void BulkInsertRecords(IEnumerable<TRecord> records, bool keepIdentities, bool hasArchiveTable, int bulkCopyTimeoutInSeconds = 3600, int batchSize = 1000, Action<string> batchLogger = null)
 		{
 			if (hasArchiveTable && !keepIdentities)
 			{
@@ -70,7 +70,7 @@ namespace ISI.Extensions.Repository.SqlServer
 					));
 				}
 			}
-			
+
 			using (var connection = GetSqlConnection())
 			{
 				connection.Open();
@@ -96,7 +96,7 @@ namespace ISI.Extensions.Repository.SqlServer
 					{
 						columns.RemoveAt(0);
 					}
-					
+
 					columns.Insert(0, new ISI.Extensions.DataReader.EnumerableDataReader<TRecord>.ColumnInfo(
 						ArchiveTableArchiveDateTimeColumnName,
 						record => false,
@@ -112,7 +112,12 @@ namespace ISI.Extensions.Repository.SqlServer
 						target.BatchSize = batchSize;
 
 						target.NotifyAfter = batchSize;
-						target.SqlRowsCopied += (sender, args) => Console.WriteLine("{0} records inserted into archive", args.RowsCopied);
+						if (batchLogger != null)
+						{
+							target.SqlRowsCopied += (sender, args) => batchLogger(string.Format("{0} records inserted into archive", args.RowsCopied));
+
+						}
+						//target.SqlRowsCopied += (sender, args) => Console.WriteLine("{0} records inserted into archive", args.RowsCopied);
 						target.WriteToServer(dataReader);
 					}
 				}

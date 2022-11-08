898fb636baec2d71c4a027c88b6b4257010ec795
Ron Muth <ron.muth@isi-net.com>
2022-11-08 14:21:15 -0500

    WIP

--- src/ISI.Extensions.VisualStudio/PackagerApi/_GetBinDirectory.cs
+++ src/ISI.Extensions.VisualStudio/PackagerApi/_GetBinDirectory.cs
@@ -12,7 +12,7 @@ Redistribution and use in source and binary forms, with or without modification,
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 #endregion
- 
+
 using ISI.Extensions.Extensions;
 using Microsoft.Extensions.Logging;
 using System;
@@ -30,7 +30,7 @@ namespace ISI.Extensions.VisualStudio
 		{
 			var projectDirectoryFullName = System.IO.Path.GetDirectoryName(projectFullName);
 
-			foreach (var framework in new[] { "net6.0", "net5.0" })
+			foreach (var framework in new[] { "net7.0", "net6.0", "net5.0" })
 			{
 				var binDirectory = System.IO.Path.Combine(projectDirectoryFullName, "bin", configuration, framework);
 

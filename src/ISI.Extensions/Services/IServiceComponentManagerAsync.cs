2582acfb2ceb589a80987ed24d759c9522b01903
Ron Muth <ron.muth@isi-net.com>
2023-01-16 14:09:41 -0500

    WIP

--- src/ISI.Extensions/Services/IServiceComponentManagerAsync.cs
+++ src/ISI.Extensions/Services/IServiceComponentManagerAsync.cs
@@ -12,7 +12,7 @@ Redistribution and use in source and binary forms, with or without modification,
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 #endregion
-
+ 
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,14 +21,14 @@ using System.Threading.Tasks;
 
 namespace ISI.Extensions.Services
 {
-	public interface IServiceComponentManager
+	public interface IServiceComponentManagerAsync
 	{
-		bool Start(IServiceContext serviceContext);
-		bool Stop(IServiceContext serviceContext);
+		Task<bool> StartAsync(IServiceContext serviceContext);
+		Task<bool> StopAsync(IServiceContext serviceContext);
 
-		void BeforeInstall(IServiceContext serviceContext);
-		void AfterInstall(IServiceContext serviceContext);
-		void BeforeUninstall(IServiceContext serviceContext);
-		void AfterUninstall(IServiceContext serviceContext);
+		Task BeforeInstallAsync(IServiceContext serviceContext);
+		Task AfterInstallAsync(IServiceContext serviceContext);
+		Task BeforeUninstallAsync(IServiceContext serviceContext);
+		Task AfterUninstallAsync(IServiceContext serviceContext);
 	}
 }

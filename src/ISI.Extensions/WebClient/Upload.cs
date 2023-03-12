722b8314f7971dc88b6fec1c4789c12511a0a85a
Ron Muth <ron.muth@isi-net.com>
2023-03-11 23:35:41 -0500

    WIP

--- src/ISI.Extensions/WebClient/Upload.cs
+++ src/ISI.Extensions/WebClient/Upload.cs
@@ -12,18 +12,35 @@ Redistribution and use in source and binary forms, with or without modification,
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 #endregion
- 
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ISI.Extensions.Extensions;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace ISI.Extensions.WebClient
 {
 	public class Upload
 	{
+		private static ISI.Extensions.Serialization.ISerialization _serialization = null;
+		protected static ISI.Extensions.Serialization.ISerialization Serialization => _serialization ??= GetSerialization();
+
+		private static ISI.Extensions.Serialization.ISerialization GetSerialization()
+		{
+			var serialization = ISI.Extensions.ServiceLocator.Current?.GetService<ISI.Extensions.Serialization.ISerialization>();
+
+			if (serialization == null)
+			{
+				throw new("ISI.Extensions.Serialization.ISerialization is null");
+			}
+
+			return serialization;
+		}
+
+
 		public static void UploadFileByChunk(string url, HeaderCollection headers, System.IO.Stream stream, string fileName, int maxFileSegmentSize = 1000000, int maxTries = 3)
 		{
 			UploadFileByChunk(new Uri(url), headers, stream, fileName, maxFileSegmentSize, maxTries);
@@ -121,12 +138,12 @@ namespace ISI.Extensions.WebClient
 			}
 		}
 
-		public static void UploadFile(string url, HeaderCollection headers, System.IO.Stream stream, string fileName, string fileFieldName = "uploadFile", System.Collections.Specialized.NameValueCollection formValues = null)
+		public static (System.Net.HttpStatusCode HttpStatusCode, string Body) UploadFile(string url, HeaderCollection headers, System.IO.Stream stream, string fileName, string fileFieldName = "uploadFile", System.Collections.Specialized.NameValueCollection formValues = null)
 		{
-			UploadFile(new Uri(url), headers, stream, fileName, fileFieldName, formValues);
+			return UploadFile(new Uri(url), headers, stream, fileName, fileFieldName, formValues);
 		}
 
-		public static void UploadFile(Uri uri, HeaderCollection headers, System.IO.Stream stream, string fileName, string fileFieldName = "uploadFile", System.Collections.Specialized.NameValueCollection formValues = null)
+		public static (System.Net.HttpStatusCode HttpStatusCode, string Body) UploadFile(Uri uri, HeaderCollection headers, System.IO.Stream stream, string fileName, string fileFieldName = "uploadFile", System.Collections.Specialized.NameValueCollection formValues = null)
 		{
 			stream.Rewind();
 
@@ -196,17 +213,122 @@ namespace ISI.Extensions.WebClient
 
 			using (var response = webRequest.GetResponse())
 			{
-				if ((response is System.Net.HttpWebResponse webResponse) && (webResponse.StatusCode != System.Net.HttpStatusCode.OK))
+				if (response is System.Net.HttpWebResponse httpWebResponse)
 				{
-					var encoding = Encoding.GetEncoding(webResponse.CharacterSet);
+					var encoding = Encoding.GetEncoding(httpWebResponse.CharacterSet);
 
-					using (var responseStream = new System.IO.StreamReader(webResponse.GetResponseStream(), encoding))
+					using (var responseStream = new System.IO.StreamReader(httpWebResponse.GetResponseStream(), encoding))
 					{
-						var description = responseStream.ReadToEnd();
+						var body = responseStream.ReadToEnd();
 
-						throw new(string.Format("{0}: {1}\n{2}", webResponse.StatusCode, webResponse.StatusDescription, description));
+						if (httpWebResponse.StatusCode != System.Net.HttpStatusCode.OK)
+						{
+							throw new(string.Format("{0}: {1}\n{2}", httpWebResponse.StatusCode, httpWebResponse.StatusDescription, body));
+						}
+
+						return (HttpStatusCode: httpWebResponse.StatusCode, Body: body);
 					}
 				}
+
+				return (HttpStatusCode: System.Net.HttpStatusCode.Ambiguous, Body: string.Empty);
+			}
+		}
+
+		public static TResponse UploadFile<TResponse>(string url, HeaderCollection headers, System.IO.Stream stream, string fileName, string fileFieldName = "uploadFile", System.Collections.Specialized.NameValueCollection formValues = null)
+			where TResponse : class
+		{
+			return UploadFile<TResponse>(new Uri(url), headers, stream, fileName, fileFieldName, formValues);
+		}
+
+		public static TResponse UploadFile<TResponse>(Uri uri, HeaderCollection headers, System.IO.Stream stream, string fileName, string fileFieldName = "uploadFile", System.Collections.Specialized.NameValueCollection formValues = null)
+			where TResponse : class
+		{
+			stream.Rewind();
+
+			var boundary = string.Format("---------------------------{0}", Guid.NewGuid().Formatted(GuidExtensions.GuidFormat.Base36));
+			var boundarybytes = System.Text.Encoding.UTF8.GetBytes(string.Format("\r\n--{0}\r\n", boundary));
+
+			var formItems = new List<byte[]>();
+			long requestLen = 0;
+
+			if (formValues != null)
+			{
+				foreach (var key in formValues.AllKeys)
+				{
+					var itemString = string.Format("Content-Disposition: form-data; name=\"{0}\"\r\n\r\n{1}", key, formValues[key]);
+					var itemBytes = System.Text.Encoding.UTF8.GetBytes(itemString);
+
+					requestLen += itemBytes.Length;
+					requestLen += boundarybytes.Length;
+
+					formItems.Add(itemBytes);
+				}
+			}
+
+
+			var fileHeader = string.Format("Content-Disposition: form-data; name=\"{0}\"; filename=\"{1}\"\r\nContent-Type: {2}\r\n\r\n", fileFieldName, fileName, ISI.Extensions.MimeType.GetMimeType(fileName));
+			var fileHeaderBytes = System.Text.Encoding.UTF8.GetBytes(fileHeader);
+			var fileTrailer = System.Text.Encoding.UTF8.GetBytes(string.Format("\r\n--{0}--\r\n", boundary));
+
+			requestLen += boundarybytes.Length + fileHeaderBytes.Length + fileTrailer.Length + stream.Length;
+
+			var webRequest = (System.Net.HttpWebRequest)System.Net.WebRequest.Create(uri);
+
+			headers?.ApplyToWebRequest(webRequest);
+
+			if (webRequest.Headers["Content-Type"] != null)
+			{
+				webRequest.Headers.Remove("Content-Type");
+			}
+
+			webRequest.ContentType = "multipart/form-data; boundary=" + boundary;
+
+			webRequest.Method = System.Net.WebRequestMethods.Http.Post;
+			webRequest.ContentLength = requestLen;
+
+			//webRequest.Expect = string.Empty;
+			//webRequest.Timeout = 1000 * 5;
+			//webRequest.ReadWriteTimeout = 1000 * 5;
+
+			using (var requestStream = webRequest.GetRequestStream())
+			{
+				foreach (var item in formItems)
+				{
+					requestStream.Write(boundarybytes, 0, boundarybytes.Length);
+					requestStream.Write(item, 0, item.Length);
+				}
+
+				requestStream.Write(boundarybytes, 0, boundarybytes.Length);
+				requestStream.Write(fileHeaderBytes, 0, fileHeaderBytes.Length);
+
+				var chunkSize = 1427; // any larger will cause an SSL request to fail
+				stream.CopyTo(requestStream, chunkSize: chunkSize);
+
+				requestStream.Write(fileTrailer, 0, fileTrailer.Length);
+
+				requestStream.Flush();
+			}
+
+			using (var response = webRequest.GetResponse())
+			{
+				if (response is System.Net.HttpWebResponse httpWebResponse)
+				{
+					var encoding = Encoding.GetEncoding(httpWebResponse.CharacterSet);
+
+					using (var responseStream = new System.IO.StreamReader(httpWebResponse.GetResponseStream(), encoding))
+					{
+						var body = responseStream.ReadToEnd();
+
+						if (httpWebResponse.StatusCode != System.Net.HttpStatusCode.OK)
+						{
+							throw new(string.Format("{0}: {1}\n{2}", httpWebResponse.StatusCode, httpWebResponse.StatusDescription, body));
+						}
+
+						return Serialization.Deserialize<TResponse>(body, ISI.Extensions.Serialization.SerializationFormat.Json);
+					}
+				}
+
+				return null;
 			}
 		}
 	}

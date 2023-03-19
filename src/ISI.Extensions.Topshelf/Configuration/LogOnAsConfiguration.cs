417966ded12b5b8a82c7b08d3ee58ac2782f6a26
Ron Muth <ron.muth@isi-net.com>
2023-03-19 15:03:49 -0400

    WIP

--- src/ISI.Extensions.Topshelf/Configuration/LogOnAsConfiguration.cs
+++ src/ISI.Extensions.Topshelf/Configuration/LogOnAsConfiguration.cs
@@ -23,7 +23,7 @@ namespace ISI.Extensions.Topshelf
 {
 	public partial class Configuration
 	{
-		public partial class LogOnAsConfiguration
+		public class LogOnAsConfiguration
 		{
 			public string UserName { get; set; }
 			public string Password { get; set; }

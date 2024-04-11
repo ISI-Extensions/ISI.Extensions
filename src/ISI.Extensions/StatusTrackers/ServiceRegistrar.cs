f804562e1393e72a39936d884b93eaefc412b06c
Ron Muth <ron.muth@isi-net.com>
2024-04-11 17:19:30 -0400

    WIP

--- src/ISI.Extensions/StatusTrackers/ServiceRegistrar.cs
+++ src/ISI.Extensions/StatusTrackers/ServiceRegistrar.cs
@@ -1,4 +1,4 @@
-#region Copyright & License
+﻿#region Copyright & License
 /*
 Copyright (c) 2024, Integrated Solutions, Inc.
 All rights reserved.
@@ -12,21 +12,34 @@ Redistribution and use in source and binary forms, with or without modification,
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 #endregion
- 
+
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+using ISI.Extensions.ConfigurationHelper.Extensions;
+using ISI.Extensions.Extensions;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace ISI.Extensions.StatusTrackers
 {
-	[ISI.Extensions.ConfigurationHelper.Configuration(ConfigurationSectionName)]
-	public partial class Configuration : ISI.Extensions.ConfigurationHelper.IConfiguration
+	[ISI.Extensions.DependencyInjection.ServiceRegistrar]
+	public class ServiceRegistrar : ISI.Extensions.DependencyInjection.IServiceRegistrarWithConfigurationRoot
 	{
-		public const string ConfigurationSectionName = "ISI.Extensions.StatusTrackers";
+		public void ServiceRegister(Microsoft.Extensions.DependencyInjection.IServiceCollection services)
+		{
+			throw new NotImplementedException();
+		}
 
-		[ISI.Extensions.EnvironmentConfigurationVariableName("ISI_STATUS_TRACKER_FACTORY_IMPLEMENTATION")]
-		public string StatusTrackerFactoryImplementation { get; set; } = nameof(FileStatusTrackerFactory);
+		public void ServiceRegister(Microsoft.Extensions.DependencyInjection.IServiceCollection services, Microsoft.Extensions.Configuration.IConfigurationRoot configurationRoot)
+		{
+			var configuration = configurationRoot.GetConfiguration<Configuration>();
 
-		public string FileStatusTrackerDirectory { get; set; } = "FileStatusTrackerDirectory";
+			if (string.Equals(configuration.StatusTrackerFactoryImplementation, nameof(FileStatusTrackerFactory), StringComparison.InvariantCultureIgnoreCase))
+			{
+				services.AddSingleton<IStatusTrackerFactory, FileStatusTrackerFactory>();
+			}
+		}
 	}
 }

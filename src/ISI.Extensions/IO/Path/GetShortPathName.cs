00a36c4ca8c829b23af2cb25aca11c48cb156729
Ron Muth <ron.muth@isi-net.com>
2024-09-28 16:42:19 -0400

    added GetShortPathName

--- src/ISI.Extensions/IO/Path/GetShortPathName.cs
+++ src/ISI.Extensions/IO/Path/GetShortPathName.cs
@@ -13,17 +13,30 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
 */
 #endregion
  
+using ISI.Extensions.Extensions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using ISI.Extensions.Extensions;
 
-namespace ISI.Extensions.VisualStudio.DataTransferObjects.CodeSigningApi
+namespace ISI.Extensions
 {
-	public class GetSignAssemblyCommandRequest : SignRequest, ISignAssembliesRequest
+	public partial class IO
 	{
-		public bool UseShortPathName { get; set; }
+		public partial class Path
+		{
+			[System.Runtime.InteropServices.DllImport("kernel32.dll", CharSet = System.Runtime.InteropServices.CharSet.Auto)]
+			public static extern int GetShortPathName([System.Runtime.InteropServices.MarshalAs(System.Runtime.InteropServices.UnmanagedType.LPTStr)] string path, [System.Runtime.InteropServices.MarshalAs(System.Runtime.InteropServices.UnmanagedType.LPTStr)] StringBuilder shortPath, int shortPathLength);
+
+			public static string GetShortPathName(string pathName)
+			{
+				var shortPathName = new StringBuilder(255);
+				
+				GetShortPathName(pathName, shortPathName, shortPathName.Capacity);
+
+				return shortPathName.ToString();
+			}
+		}
 	}
 }

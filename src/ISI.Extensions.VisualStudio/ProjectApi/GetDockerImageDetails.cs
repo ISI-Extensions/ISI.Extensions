0af51cfdc7882020bc35e89366d6fa9c49a402ea
Ron Muth <ron.muth@isi-net.com>
2024-12-04 21:53:16 -0500

    WIP

--- src/ISI.Extensions.VisualStudio/ProjectApi/GetDockerImageDetails.cs
+++ src/ISI.Extensions.VisualStudio/ProjectApi/GetDockerImageDetails.cs
@@ -12,7 +12,7 @@ Redistribution and use in source and binary forms, with or without modification,
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 #endregion
- 
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -50,10 +50,13 @@ namespace ISI.Extensions.VisualStudio
 						response.ContainerRegistry = propertyGroupElement.GetElementByLocalName("ContainerRegistry")?.Value ?? string.Empty;
 						response.ContainerRepository = propertyGroupElement.GetElementByLocalName("ContainerRepository")?.Value ?? propertyGroupElement.GetElementByLocalName("ContainerImageName")?.Value ?? string.Empty;
 						response.ContainerImageTags = (propertyGroupElement.GetElementByLocalName("ContainerImageTags")?.Value ?? propertyGroupElement.GetElementByLocalName("ContainerImageTag")?.Value ?? "latest").Split(';');
+						response.ContainerFamily = propertyGroupElement.GetElementByLocalName("ContainerFamily")?.Value ?? string.Empty;
+						response.ContainerBaseImage = propertyGroupElement.GetElementByLocalName("ContainerBaseImage")?.Value ?? string.Empty;
+						response.ContainerWorkingDirectory = propertyGroupElement.GetElementByLocalName("ContainerWorkingDirectory")?.Value ?? string.Empty;
 					}
 				}
 			}
-			
+
 			return response;
 		}
 	}

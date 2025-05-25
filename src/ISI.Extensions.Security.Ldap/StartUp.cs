20deacab65d715d05d4dd9ace03c9343ecc4e02b
Ron Muth <ron.muth@isi-net.com>
2025-05-25 15:56:15 -0400

    add debug

--- src/ISI.Extensions.Security.Ldap/StartUp.cs
+++ src/ISI.Extensions.Security.Ldap/StartUp.cs
@@ -12,46 +12,47 @@ Redistribution and use in source and binary forms, with or without modification,
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 #endregion
-
+ 
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ISI.Extensions.Extensions;
-using ISI.Extensions.Security.Ldap.Extensions;
-using Microsoft.Extensions.Logging;
-using DTOs = ISI.Extensions.Security.Ldap.DataTransferObjects.LdapApi;
 
 namespace ISI.Extensions.Security.Ldap
 {
-	public partial class LdapApi
+	[ISI.Extensions.StartUp]
+	public class StartUp : ISI.Extensions.IStartUp
 	{
-		public DTOs.AuthenticateUserResponse AuthenticateUser(DTOs.AuthenticateUserRequest request)
+		private static bool _isInitialized = false;
+		public void Start()
 		{
-			var response = new DTOs.AuthenticateUserResponse();
-
-			try
+			if (!_isInitialized)
 			{
-				using (var ldapConnection = request.GetLdapConnection(MemoryCache))
-				{
-					var fqdn = ldapConnection.GetFQDN();
+				_isInitialized = true;
 
-					var userName = $"{request.UserName.Split(new[] { '\\', '/' }).Last()}@{fqdn}";
+				//if (System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(System.Runtime.InteropServices.OSPlatform.Linux))
+				//{
+				//	System.Runtime.Loader.AssemblyLoadContext.Default.ResolvingUnmanagedDll += (assembly, fileName) =>
+				//	{
+				//		const string ldapFileName = "libldap-2.5.so.0";
 
-					ldapConnection.BindAsync(userName, request.Password).GetAwaiter().GetResult();
-
-					response.Authenticated = true;
-				}
+				//		if (!fileName.Equals(ldapFileName))
+				//		{
+				//			return IntPtr.Zero;
+				//		}
+				//		try
+				//		{
+				//			return System.Runtime.InteropServices.NativeLibrary.Load(ldapFileName);
+				//		}
+				//		catch (Exception)
+				//		{
+				//			return System.Runtime.InteropServices.NativeLibrary.Load("libldap.so.2");
+				//		}
+				//	};
+				//}
 			}
-			catch (Exception exception)
-			{
-//#if DEBUG
-				Logger.LogError(exception, $"AuthenticateUser\nHost:{request.LdapHost}\nPort:{request.LdapPort}\nMessage:{exception.Message}");
-//#endif
-			}
-
-			return response;
 		}
 	}
 }

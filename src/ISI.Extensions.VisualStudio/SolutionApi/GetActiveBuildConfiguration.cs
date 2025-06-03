81f6edfe34207a0bdc652c637948bf4cd1dcbc7f
Ron Muth <ron.muth@isi-net.com>
2025-06-03 16:18:36 -0400

    upgrade nuget packages

--- src/ISI.Extensions.VisualStudio/SolutionApi/GetActiveBuildConfiguration.cs
+++ src/ISI.Extensions.VisualStudio/SolutionApi/GetActiveBuildConfiguration.cs
@@ -12,7 +12,7 @@ Redistribution and use in source and binary forms, with or without modification,
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 #endregion
- 
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -50,7 +50,7 @@ namespace ISI.Extensions.VisualStudio
 							var nameAttribute = platformElement.GetAttributeByLocalName("Name");
 							if (nameAttribute != null)
 							{
-								buildConfigurations.Add(new (nameAttribute.Value));
+								buildConfigurations.Add(new(nameAttribute.Value));
 							}
 						}
 					}
@@ -88,39 +88,36 @@ namespace ISI.Extensions.VisualStudio
 						{
 							var configurationFileName = configurationFiles.First();
 
-							var configuration = new OpenMcdf.CompoundFile(configurationFileName);
-
-							var storageStream = configuration.RootStorage.GetStream("SolutionConfiguration");
-
-							var data = storageStream.GetData();
-
-							using (var stream = new System.IO.MemoryStream(data))
+							using (var configuration = OpenMcdf.RootStorage.OpenRead(configurationFileName))
 							{
-								using (var streamReader = new System.IO.StreamReader(stream, Encoding.Unicode))
+								using (var stream = configuration.OpenStream("SolutionConfiguration"))
 								{
-									var config = streamReader.ReadToEnd();
-
-									var configArray = config.ToCharArray();
-
-									for (var i = 0; i < configArray.Length; i++)
+									using (var streamReader = new System.IO.StreamReader(stream, Encoding.Unicode))
 									{
-										if (configArray[i] < 32)
+										var config = streamReader.ReadToEnd();
+
+										var configArray = config.ToCharArray();
+
+										for (var i = 0; i < configArray.Length; i++)
 										{
-											configArray[i] = ' ';
+											if (configArray[i] < 32)
+											{
+												configArray[i] = ' ';
+											}
 										}
-									}
 
-									config = new(configArray);
+										config = new(configArray);
 
-									var parameters = config.Split(new[] { ';' }).Select(item => item.Split(new[] { '=' }).Select(piece => piece.Trim()).ToArray()).ToArray().Where(item => item.Length == 2).ToDictionary(pieces => pieces[0], pieces => pieces[1]);
+										var parameters = config.Split(new[] { ';' }).Select(item => item.Split(new[] { '=' }).Select(piece => piece.Trim()).ToArray()).ToArray().Where(item => item.Length == 2).ToDictionary(pieces => pieces[0], pieces => pieces[1]);
 
-									if (parameters.TryGetValue("ActiveCfg", out var msBuildConfiguration))
-									{
-										var buildConfiguration = buildConfigurations.FirstOrDefault(buildConfiguration => string.Equals(buildConfiguration.ToString(), msBuildConfiguration, StringComparison.CurrentCultureIgnoreCase));
-
-										if (buildConfiguration != null)
+										if (parameters.TryGetValue("ActiveCfg", out var msBuildConfiguration))
 										{
-											response.ActiveBuildConfiguration = buildConfiguration;
+											var buildConfiguration = buildConfigurations.FirstOrDefault(buildConfiguration => string.Equals(buildConfiguration.ToString(), msBuildConfiguration, StringComparison.CurrentCultureIgnoreCase));
+
+											if (buildConfiguration != null)
+											{
+												response.ActiveBuildConfiguration = buildConfiguration;
+											}
 										}
 									}
 								}

55e3860b19869d9971e5d4bd70fb3bba59783f90
Ron Muth <ron.muth@isi-net.com>
2025-11-07 18:00:28 -0500

    WIP - Master connection string

--- src/ISI.Extensions.Repository.Oracle/RepositorySetupApi/_GetMasterConnectionString.cs
+++ src/ISI.Extensions.Repository.Oracle/RepositorySetupApi/_GetMasterConnectionString.cs
@@ -49,6 +49,14 @@ namespace ISI.Extensions.Repository.Oracle
 
 			if (connectionString.Split(';', StringSplitOptions.RemoveEmptyEntries).Length <= 1)
 			{
+				if ((Configuration as IConfigurationRoot)?.TryGetConnectionString(connectionString, out var masterConnectionString) ?? false)
+				{
+					connectionString = masterConnectionString;
+				}
+			}
+
+			if (connectionString.Split(';', StringSplitOptions.RemoveEmptyEntries).Length <= 1)
+			{
 				return connectionString;
 			}
 

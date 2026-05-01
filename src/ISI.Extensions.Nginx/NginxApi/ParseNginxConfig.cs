a64c65cf34feb7c7cdd8e1a540dbba1bc6b41906
Ron Muth <ron.muth@isi-net.com>
2026-05-01 14:06:52 -0400

    ISI.Extensions.Nginx

---
+++ src/ISI.Extensions.Nginx/NginxApi/ParseNginxConfig.cs
@@ -0,0 +1,116 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ISI.Extensions.Extensions;
+using DTOs = ISI.Extensions.Nginx.DataTransferObjects.NginxApi;
+
+namespace ISI.Extensions.Nginx
+{
+	public partial class NginxApi
+	{
+		public DTOs.ParseNginxConfigResponse ParseNginxConfig(DTOs.ParseNginxConfigRequest request)
+		{
+			var response = new DTOs.ParseNginxConfigResponse();
+
+			response.ParsedNginxConfig = new()
+			{
+				FileName = request.FileName,
+				Content = request.Content,
+			};
+
+			var servers = new List<ParsedNginxConfigServer>();
+
+			var serverRegex = new System.Text.RegularExpressions.Regex(@"(?:\s*)(?<server>server)(?:\s*)(?:\{?)(?:\s*)");
+			var listenRegex = new System.Text.RegularExpressions.Regex(@"(?:\s*listen\s*(?<port>\d+)(?:\s*(?<scheme>[a-zA-Z]+))?)");
+			var serverNameRegex = new System.Text.RegularExpressions.Regex(@"(?:\s*)(?<server_name>server_name)(?:\s+)(?<host>[a-zA-Z0-9\-\.]+)(?:\s*)");
+			var locationRegex = new System.Text.RegularExpressions.Regex(@"(?:\s*)(?<location>location)(?:\s+)(?<directory>[^ ]+)(?:\s*)");
+			var proxyPassRegex = new System.Text.RegularExpressions.Regex(@"(?:\s*)(?<proxy_pass>proxy_pass)(?:\s+)(?<url>[^ ;]+)(?:\s*)");
+
+			{
+				var server = (ParsedNginxConfigServer)null;
+				foreach (var line in request.Content.Replace("\r\n", "\n").Split('\n'))
+				{
+					if (serverRegex.Match(line).Success)
+					{
+						server = new ParsedNginxConfigServer();
+						servers.Add(server);
+					}
+
+					server?.Content += $"\n{line}";
+				}
+
+				response.ParsedNginxConfig.Servers = servers.ToArray();
+			}
+
+			foreach (var server in servers)
+			{
+				server.Content = server.Content.Trim([' ', '\n']);
+
+				var locations = new List<ParsedNginxConfigServerLocation>();
+
+				var location = (ParsedNginxConfigServerLocation)null;
+				var scheme = (string)null;
+				foreach (var line in server.Content.Split('\n'))
+				{
+					if (location == null)
+					{
+						var listenMatch = listenRegex.Match(line);
+
+						if (listenMatch.Success)
+						{
+							server.Ssl = (string.Equals(listenMatch.Groups["scheme"]?.Value ?? string.Empty, "ssl", StringComparison.InvariantCultureIgnoreCase));
+							scheme = (string.Equals(listenMatch.Groups["scheme"]?.Value ?? string.Empty, "ssl", StringComparison.InvariantCultureIgnoreCase) ? Uri.UriSchemeHttps : Uri.UriSchemeHttp);
+
+							server.Port = (listenMatch.Groups["port"]?.Value ?? string.Empty).ToIntNullable() ?? (string.Equals(scheme, Uri.UriSchemeHttps, StringComparison.InvariantCultureIgnoreCase) ? 443 : 80);
+						}
+
+						if (string.IsNullOrWhiteSpace(scheme) && (line.IndexOf("ssl-cert.inc", StringComparison.InvariantCultureIgnoreCase) >= 0))
+						{
+							server.Port = 443;
+						}
+
+						var serverNameMatch = serverNameRegex.Match(line);
+
+						if (serverNameMatch.Success)
+						{
+							server.Host = serverNameMatch.Groups["host"]?.Value ?? string.Empty;
+						}
+					}
+
+					var locationMatch = locationRegex.Match(line);
+
+					if (locationMatch.Success)
+					{
+						location = new ParsedNginxConfigServerLocation();
+						locations.Add(location);
+
+						location.Location = locationMatch.Groups["directory"]?.Value ?? string.Empty;
+					}
+
+					if (location != null)
+					{
+						var proxyPassMatch = proxyPassRegex.Match(line);
+
+						if (proxyPassMatch.Success)
+						{
+							location.ProxyPassUrl = proxyPassMatch.Groups["url"]?.Value ?? string.Empty;
+						}
+
+						location?.Content += $"\n{line}";
+					}
+				}
+
+				foreach (var serverLocation in locations)
+				{
+					serverLocation.Content = serverLocation.Content.Trim([' ', '\n']);
+				}
+
+				server.Locations = locations.ToArray();
+			}
+
+			return response;
+		}
+	}
+}
---
+++ src/ISI.Extensions.Nginx/ParsedNginxConfig.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ISI.Extensions.Nginx
+{
+	public class ParsedNginxConfig
+	{
+		public string FileName { get; set; }
+		public string Content { get; set; }
+
+		public ParsedNginxConfigServer[] Servers { get; set; }
+	}
+
+	public class ParsedNginxConfigServer
+	{
+		public string Host { get; set; }
+		public int Port { get; set; }
+		public bool Ssl { get; set; }
+
+		public ParsedNginxConfigServerLocation[] Locations { get; set; }
+
+		public string Content { get; set; }
+	}
+
+	public class ParsedNginxConfigServerLocation
+	{
+		public string Location { get; set; }
+		public string ProxyPassUrl { get; set; }
+
+		public string Content { get; set; }
+	}
+}

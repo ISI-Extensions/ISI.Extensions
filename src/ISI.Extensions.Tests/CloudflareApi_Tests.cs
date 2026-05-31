233541718459f3d1c7221e9788cdab30f5cedecf
Ron Muth <ron.muth@isi-net.com>
2026-05-31 11:28:55 -0400

    fix EnsureZoneId

--- src/ISI.Extensions.Tests/CloudflareApi_Tests.cs
+++ src/ISI.Extensions.Tests/CloudflareApi_Tests.cs
@@ -12,18 +12,18 @@ Redistribution and use in source and binary forms, with or without modification,
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 #endregion
- 
-using ISI.Extensions.ConfigurationHelper.Extensions;
-using ISI.Extensions.DependencyInjection.Extensions;
-using ISI.Extensions.Extensions;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using NUnit.Framework;
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using ISI.Extensions.ConfigurationHelper.Extensions;
+using ISI.Extensions.DependencyInjection.Extensions;
+using ISI.Extensions.Extensions;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NUnit.Framework;
 
 namespace ISI.Extensions.Tests
 {
@@ -130,11 +130,12 @@ namespace ISI.Extensions.Tests
 
 			using (var eventHandler = ISI.Extensions.WebClient.Rest.GetEventHandler())
 			{
-				var listDnsRecordsResponse = cloudflareApi.SetDnsRecords(new()
+				var setDnsRecordsResponse = cloudflareApi.SetDnsRecords(new()
 				{
 					ApiToken = settings.GetValue("CloudFlare.ApiKey"),
 					ZoneName = "ronmuth.com",
-					DnsRecords = [
+					DnsRecords =
+					[
 						new ISI.Extensions.Dns.DnsRecord()
 						{
 							RecordType = ISI.Extensions.Dns.RecordType.CanonicalNameRecord,
@@ -143,17 +144,17 @@ namespace ISI.Extensions.Tests
 						},
 						new ISI.Extensions.Dns.DnsRecord()
 						{
+							RecordType = ISI.Extensions.Dns.RecordType.TextRecord,
+							Name = "_ron",
+							Data = "HappenedAgain!"
+						},
+						new ISI.Extensions.Dns.DnsRecord()
+						{
 							RecordType = ISI.Extensions.Dns.RecordType.AddressRecord,
 							Name = "git",
 							Data = "10.105.0.11"
 						},
-						new ISI.Extensions.Dns.DnsRecord()
-						{
-							RecordType = ISI.Extensions.Dns.RecordType.TextRecord,
-							Name = "_ron",
-							Data = "HappenedAgain!"
-						}
-						],
+					],
 				});
 			}
 		}

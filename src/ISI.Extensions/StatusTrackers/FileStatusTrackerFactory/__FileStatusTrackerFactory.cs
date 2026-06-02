fa86614fcc9f3f9cb9145511487fcba4caa3246c
Ron Muth <ron.muth@isi-net.com>
2026-06-02 09:02:35 -0400

    WIP

--- src/ISI.Extensions/StatusTrackers/FileStatusTrackerFactory/__FileStatusTrackerFactory.cs
+++ src/ISI.Extensions/StatusTrackers/FileStatusTrackerFactory/__FileStatusTrackerFactory.cs
@@ -115,7 +115,7 @@ namespace ISI.Extensions.StatusTrackers
 			return false;
 		}
 
-		public virtual IStatusTracker CreateStatusTracker(string statusTrackerKey, string jobKey, string description, string logUrl) => CreateStatusTracker(statusTrackerKey);
+		public virtual IStatusTracker CreateStatusTracker(string statusTrackerKey, string jobKey, string description, string lastExecutionUrl) => CreateStatusTracker(statusTrackerKey);
 		public IStatusTracker CreateStatusTracker(string statusTrackerKey)
 		{
 			return new FileStatusTracker(statusTrackerKey, Configuration, Logger, DateTimeStamper, GetStatusTrackerFileName, GetStatusTrackerKeyValueFileName);

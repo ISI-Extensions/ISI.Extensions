38db76b9843b353e02a34e70cbd05269c75b4cfb
Ron Muth <ron.muth@isi-net.com>
2026-06-02 08:07:48 -0400

    JobExecutionStatus

---
+++ src/ISI.Extensions/StatusTrackers/JobExecutionStatus.cs
@@ -0,0 +1,30 @@
+﻿#region Copyright & License
+/*
+Copyright (c) 2026, Integrated Solutions, Inc.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+		* Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+		* Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+		* Neither the name of the Integrated Solutions, Inc. nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+#endregion
+ 
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ISI.Extensions.StatusTrackers
+{
+	public enum JobExecutionStatus
+	{
+		[ISI.Extensions.EnumGuid("9baaf1ae-2552-408a-8547-745a255ca57e")] New,
+		[ISI.Extensions.EnumGuid("d59cf129-75a8-43d8-a432-1e2bb62a97b0")] Running,
+		[ISI.Extensions.EnumGuid("b781dbc5-aef5-4d01-8aea-9696468f19d3")] Finished,
+		[ISI.Extensions.EnumGuid("61bd41fe-0212-4cb3-8e15-cad84233e918")] Failed,
+		[ISI.Extensions.EnumGuid("56f2e24e-5613-4977-a373-d33749486772")] TimedOut,
+	}
+}

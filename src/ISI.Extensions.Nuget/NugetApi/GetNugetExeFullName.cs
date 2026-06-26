896508efd41aa371b2a0322d10f8f9b3864624c1
Ron Muth <ron.muth@isi-net.com>
2026-06-26 12:11:39 -0400

    download nuget.exe top tempdir

--- src/ISI.Extensions.Nuget/NugetApi/GetNugetExeFullName.cs
+++ src/ISI.Extensions.Nuget/NugetApi/GetNugetExeFullName.cs
@@ -12,7 +12,7 @@ Redistribution and use in source and binary forms, with or without modification,
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 #endregion
- 
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,9 +21,9 @@ using System.Threading.Tasks;
 using ISI.Extensions.Extensions;
 using ISI.Extensions.JsonSerialization.Extensions;
 using ISI.Extensions.Nuget.Extensions;
+using Microsoft.Extensions.Logging;
 using DTOs = ISI.Extensions.Nuget.DataTransferObjects.NugetApi;
 using SerializableDTOs = ISI.Extensions.Nuget.SerializableModels.Nuget;
-using Microsoft.Extensions.Logging;
 
 namespace ISI.Extensions.Nuget
 {
@@ -64,9 +64,11 @@ namespace ISI.Extensions.Nuget
 
 			if (string.IsNullOrWhiteSpace(_nugetExeFullName))
 			{
-				var uriCodeBase = new UriBuilder(GetType().Assembly.CodeBase);
+				//var uriCodeBase = new UriBuilder(GetType().Assembly.CodeBase);
 
-				var directory = System.IO.Path.GetDirectoryName(Uri.UnescapeDataString(uriCodeBase.Path));
+				//var directory = System.IO.Path.GetDirectoryName(Uri.UnescapeDataString(uriCodeBase.Path));
+
+				var directory = ISI.Extensions.IO.Path.GetTempDirectoryName();
 
 				var nugetExeFullName = System.IO.Path.Combine(directory, nugetExeFileName);
 
@@ -78,18 +80,20 @@ namespace ISI.Extensions.Nuget
 					}
 
 					ISI.Extensions.IO.FileZone.RemoveZone(nugetExeFullName);
+
+					_nugetExeFullName = nugetExeFullName;
 				}
+			}
 
-				if (!string.IsNullOrWhiteSpace(nugetExeFullName) && System.IO.File.Exists(nugetExeFullName))
+			if (!string.IsNullOrWhiteSpace(_nugetExeFullName) && System.IO.File.Exists(_nugetExeFullName))
+			{
+				var versionInfo = System.Diagnostics.FileVersionInfo.GetVersionInfo(_nugetExeFullName);
+				var productVersion = new System.Version(versionInfo.ProductVersion);
+				var minProductionVersion = new System.Version("5.8.1");
+
+				if (productVersion < minProductionVersion)
 				{
-					var versionInfo = System.Diagnostics.FileVersionInfo.GetVersionInfo(nugetExeFullName);
-					var productVersion = new System.Version(versionInfo.ProductVersion);
-					var minProductionVersion = new System.Version("5.8.1");
-
-					if (productVersion < minProductionVersion)
-					{
-						var processResponse = ISI.Extensions.Process.WaitForProcessResponse(nugetExeFullName, new[] { "update", "-self" });
-					}
+					var processResponse = ISI.Extensions.Process.WaitForProcessResponse(_nugetExeFullName, new[] { "update", "-self" });
 				}
 			}
 

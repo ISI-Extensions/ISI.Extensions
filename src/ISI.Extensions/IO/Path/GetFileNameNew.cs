790e23067cbb984d07dd1de0b24779a3c88e904d
Ron Muth <ron.muth@isi-net.com>
2026-07-12 08:26:11 -0400

    GetFileNameNew

--- src/ISI.Extensions/IO/Path/GetFileNameNew.cs
+++ src/ISI.Extensions/IO/Path/GetFileNameNew.cs
@@ -12,7 +12,7 @@ Redistribution and use in source and binary forms, with or without modification,
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 #endregion
- 
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -29,8 +29,8 @@ namespace ISI.Extensions
 
 			public static string GetFileNameNew(string directory, string fileName)
 			{
-				var fileNameWithoutExtension = System.IO.Path.GetFileNameWithoutExtension(fileName);
-				var fileNameExtension = System.IO.Path.GetExtension(fileName);
+				var fileNameWithoutExtension = System.IO.Path.GetFileNameWithoutExtension(fileName).TrimEnd('.');
+				var fileNameExtension = System.IO.Path.GetExtension(fileName).TrimStart('.');
 				var fileIndex = 1;
 
 				var fullName = System.IO.Path.Combine(directory, fileName);

cb076d1a1f533fb999cf213fe62275d672a42951
Ron Muth <ron.muth@isi-net.com>
2026-08-04 16:43:00 -0400

    Syslog

---
+++ src/ISI.Extensions/Syslog/Client.cs
@@ -0,0 +1,99 @@
+﻿#region Copyright & License
+/*
+Copyright (c) 2026, Integrated Solutions, Inc.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+		* Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+		* Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+		* Neither the name of the Integrated Solutions, Inc. nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ISI.Extensions.Extensions;
+
+namespace ISI.Extensions.Syslog
+{
+	public class ServerIpAddress
+	{
+		public System.Net.IPAddress IpAddress { get; set; }
+
+		public static implicit operator ServerIpAddress(string value)
+		{
+			if (!System.Net.IPAddress.TryParse(value, out var ipAddress))
+			{
+				var hostEntry = System.Net.Dns.GetHostEntry(value);
+
+				ipAddress = hostEntry.AddressList.NullCheckedFirstOrDefault();
+			}
+
+			return new ServerIpAddress()
+			{
+				IpAddress = ipAddress,
+			};
+		}
+	}
+
+	public class Client : IDisposable
+	{
+		private UdpClient _udpClient = null;
+		protected UdpClient UdpClient => _udpClient ??= new UdpClient();
+
+		private System.Net.IPAddress _clientIpAddress = null;
+		protected System.Net.IPAddress ClientIpAddress
+		{
+			get => _clientIpAddress ??= System.Net.Dns.GetHostEntry(System.Net.Dns.GetHostName()).AddressList.NullCheckedFirstOrDefault(ipAddress => ipAddress.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork);
+			set => _clientIpAddress = value;
+		}
+
+		public ServerIpAddress ServerIpAddress { get; set; }
+		public int ServerPort { get; set; } = 514;
+
+		public bool IsActive => UdpClient.IsActive;
+
+		public void Close()
+		{
+			if (UdpClient.IsActive)
+			{
+				UdpClient.Close();
+			}
+		}
+
+		public void Send(Message message)
+		{
+			if (!UdpClient.IsActive)
+			{
+				UdpClient.Connect(ServerIpAddress.IpAddress, ServerPort);
+			}
+
+			if (UdpClient.IsActive)
+			{
+				var msg = $"<{message.Priority}>{DateTime.Now:MMM dd HH:mm:ss} {ClientIpAddress.Address} {message.Text}";
+
+				var bytes = System.Text.Encoding.ASCII.GetBytes(msg);
+
+				UdpClient.Send(bytes, bytes.Length);
+			}
+			else
+			{
+				throw new Exception("Syslog client Socket did not connect");
+			}
+		}
+
+		public void Dispose()
+		{
+			if (_udpClient?.IsActive ?? false)
+			{
+				_udpClient.Close();
+			}
+
+			_udpClient?.Dispose();
+		}
+	}
+}
